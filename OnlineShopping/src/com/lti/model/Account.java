b14d611e3715a1ee73f3dee2f13882221fb8cd14
VIGANESHA <VIGANESHA@DESKTOP-1AAARUT>
2020-10-16 09:50:20 +0530

    Added Account

---
+++ OnlineShopping/src/com/lti/model/Account.java
@@ -0,0 +1,21 @@
+package com.lti.model;
+
+public class Account {
+	 int accountId;
+	 double balance;
+	public int getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public Account() {
+	
+	}
+}
