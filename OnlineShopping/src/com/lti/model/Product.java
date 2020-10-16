1e9fc721f0959b7ec7cb5d16ee3e96744a874345
VIGANESHA H R <42531234+viganeshahr@users.noreply.github.com>
2020-10-16 10:20:40 +0530

    deleted code to check conflicts

--- OnlineShopping/src/com/lti/model/Product.java
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -2,23 +2,9 @@ package com.lti.model;
 
 public class Product {
 	
-	int productId;
 	String productName;
 	String productDescription;
-	int Quantiy;
 	
-	public int getQuantitiy(){
-		return quantity;
-	}
-	public int getProductId() {
-		return productId;
-	}
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
-	public String getProductName() {
-		return productName;
-	}
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
