935c545e7d1e4be50c103aa9576bc304bf5e77c8
VIGANESHA <VIGANESHA@DESKTOP-1AAARUT>
2020-10-16 09:27:28 +0530

    Added Product Class thru br1

---
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -0,0 +1,28 @@
+package com.lti.model;
+
+public class Product {
+	
+	int productId;
+	String productName;
+	String productDescription;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	
+	
+}
